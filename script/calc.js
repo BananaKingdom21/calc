c13b2211e1289afe5819a973ada3129589985cfd
Bananaperson21 <75265949+Bananaperson21@users.noreply.github.com>
2021-01-27 13:26:40 -0500

    Update calc.js

--- script/calc.js
+++ script/calc.js
@@ -1,23 +1,10 @@
-var mathArea1 = document.getElementById("answerArea");
-var mathValue = mathArea1.value;
-var prob1 = undefined;
-var answer = 0;
+var mathArea = document.getElementById("answerArea");
+var prob;
+var answer;
 
-function solve () {
-  for (var i = 0; i < prob1.length; i += 1) {
-    if(prob1[i] === 0) {answer *= 10}
-    if(prob1[i] === 1) {answer =  answer *10 + 1}
-    if(prob1[i] === 2) {answer =  answer *10 + 2}
-    if(prob1[i] === 3) {answer =  answer *10 + 3}
-    if(prob1[i] === 4) {answer =  answer *10 + 4}
-    if(prob1[i] === 5) {answer =  answer *10 + 5}
-    if(prob1[i] === 6) {answer =  answer *10 + 6}
-    if(prob1[i] === 7) {answer =  answer *10 + 7}
-    if(prob1[i] === 8) {answer =  answer *10 + 8}
-    if(prob1[i] === 9) {answer =  answer *10 + 9}
-    
-    
-  }
+
+function solve (problem) {
+  answer = function(""+problem+"")();
 }
 
 
@@ -25,7 +12,6 @@ function calc1 () {
   requestAnimationFrame(calc1);
   
   mathValue = mathArea1.value;
-  prob1 = mathValue.split("");
   
   if (mathValue.search("=") < 0) {
   solve();
