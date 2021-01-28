f035ecb24ccc4b06e40e737fe531456b702d7d2c
Alex <74437349+BananaMaster21@users.noreply.github.com>
2021-01-27 19:18:08 -0500

    Update calc.js

--- script/calc.js
+++ script/calc.js
@@ -2,21 +2,12 @@ var mathArea = document.getElementById("answerArea");
 var someMath;
 var test = 0;
 
-function solve(math){
-    someMath.replace("=","");
-    return Function("return (" + math + ")")();
-}
+//do the math
+function solve (math){return Function("return (" + math + ")")();}
 
-function calc1 () {
-  requestAnimationFrame(calc1);
-    
-  if (someMath || someMath.search("=") === -1) {test = 1;}else {test = 0;}
-      
-  if (test === 0) {
-  someMath = mathArea.value;
-      }else {
-  mathArea.value = solve(someMath);
-      }
+function swap () {someMath = mathArea.value;someMath.replace("=","");someMath.replace("x","*");someMath.replace("÷","/");mathArea.value = solve(someMath);}
 
+mathArea.onkeydown = function(event){
+    var key = event.key;
+    if (key === "=" || key === "Enter") {swap();}
 }
-requestAnimationFrame(calc1);
